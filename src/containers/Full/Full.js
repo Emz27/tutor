cf160473262862a2d4b4208563b2b0b99c37c387
Emz27 <emmanuelbenitez1996@gmail.com>
2018-04-04 14:58:39 +0800

    fix padding on small devices

--- src/containers/Full/Full.js
+++ src/containers/Full/Full.js
@@ -15,14 +15,11 @@ import {Schedule as TutorRatings} from '../../views/Tutor/Ratings.js';
 
 import {FindTutor} from '../../views/Student/FindTutor.js';
 import {Profile as StudentProfile} from '../../views/Student/Profile.js';
-import {Schedule as StudentSchedule} from '../../views/Student/Profile.js';
+import {Schedule as StudentSchedule} from '../../views/Student/Schedule.js';
 import {Contracts as StudentContracts} from '../../views/Student/Contracts.js';
 
 class Full extends Component {
-  constructor(props){
-    super(props);
 
-  }
   render() {
     return (
       <div className="app">
@@ -31,23 +28,23 @@ class Full extends Component {
           <Sidebar {...this.props}/>
           <main className="main">
             <Breadcrumb />
-            <Container fluid>
+            <Container fluid className="p-0 p-md-3">
               {
                 (this.props.user.type === 'Tutor')?
                 <Switch>
-                  <Route path="/profile" name="Profile" component={TutorProfile}/>
-                  <Route path="/schedule" name="Schedule" component={TutorSchedule}/>
-                  <Route path="/subjects" name="Subjects" component={TutorSubjects}/>
-                  <Route path="/contracts" name="Contracts" component={TutorContracts}/>
-                  <Route path="/ratings" name="Ratings" component={TutorRatings}/>
+                  <Route path="/profile" name="Profile" render={()=><TutorProfile user={this.props.user}/>}/>
+                  <Route path="/schedule" name="Schedule" render={()=><TutorSchedule user={this.props.user}/>}/>
+                  <Route path="/subjects" name="Subjects" render={()=><TutorSubjects user={this.props.user}/>}/>
+                  <Route path="/contracts" name="Contracts" render={()=><TutorContracts user={this.props.user}/>}/>
+                  <Route path="/ratings" name="Ratings" render={()=><TutorRatings user={this.props.user}/>}/>
                   <Redirect from="/" to="/profile"/>
                 </Switch>
                 :
                 <Switch>
-                  <Route path="/find_tutor" name="Find Tutor" component={FindTutor}/>
-                  <Route path="/profile" name="Profile" component={StudentProfile}/>
-                  <Route path="/schedule" name="Schedule" component={StudentSchedule}/>
-                  <Route path="/contracts" name="Contracts" component={StudentContracts}/>
+                  <Route path="/find_tutor" name="Find Tutor" render={()=><FindTutor user={this.props.user}/>}/>
+                  <Route path="/profile" name="Profile" render={()=><StudentProfile user={this.props.user}/>}/>
+                  <Route path="/schedule" name="Schedule" render={()=><StudentSchedule user={this.props.user}/>}/>
+                  <Route path="/contracts" name="Contracts" render={()=><StudentContracts user={this.props.user}/>}/>
                   <Redirect from="/" to="/profile"/>
                 </Switch>
               }
